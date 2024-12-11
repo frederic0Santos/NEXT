ad77a7137accc33a062adc8339b10a2fe3a3d832
frederic0Santos <fred16mail@gmail.com>
2024-12-11 15:41:29 +0000

    mudei um pouco o tailwind e css

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-transparent text-white font-bold p-2.5 text-center">
+    <div className="bg-transparent text-white font-bold p-2.5 text-center my-10">
   2024 © Frederico Santos
   </div>
   )
