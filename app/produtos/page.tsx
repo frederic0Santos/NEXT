a961f86739333d679544ec5964272b513253a71c
frederic0Santos <fred16mail@gmail.com>
2024-12-12 16:28:18 +0000

    final commit i think

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import useSWR from 'swr';
 import Card from './card'; 
 import { Product } from '../models/interfaces';
